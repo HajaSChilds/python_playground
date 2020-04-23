45f31c7c1d31252fd9bb75f576261a7227611ce6
HajaSC <hajasc@protonmail.com>
2020-04-22 23:29:27 -0400

    lesson38.py Final

--- lesson38.py
+++ lesson38.py
@@ -1,8 +1,10 @@
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
-print "Wait there's not 10 things in that list, let's fix that."
+#print "Wait there's not 10 things in that list, let's fix that."
 
 stuff = ten_things.split(' ')
+
+print stuff
 
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
@@ -10,13 +12,13 @@ while len(stuff) !=10:
     next_one = more_stuff.pop()
     print "Adding: ", next_one
     stuff.append(next_one)
-    print "There's %d items now." % len(stuff)
+    print (f"There\'s {len(stuff)} items now.")  
 
-    print "There we go: ", stuff
-    print "Lt's do some things with stuff."
+print "There we go: ", stuff
+print "Let's do some things with stuff."
 
-    print stuff[1]
-    print stuff[-1]
-    print stuff.pop()
-    print ' '. join(stuff)
-    print '#'.join(stuff[3:5])
+print stuff[1]
+print stuff[-1]
+print stuff.pop()
+print ' '. join(stuff)
+print '#'.join(stuff[3:5])
